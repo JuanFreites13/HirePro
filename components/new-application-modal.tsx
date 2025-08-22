8ab82cf63483e8fc154ffea727aaae8a900f062c
JuanFreites13 <juan@agendapro.com>
2025-08-22 16:14:18 -0400

    Fix: React import errors causing application crash

--- components/new-application-modal.tsx
+++ components/new-application-modal.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-import type React from "react"
-
-import { useState } from "react"
+import React, { useState } from "react"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
--- components/new-candidate-modal.tsx
+++ components/new-candidate-modal.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-import type React from "react"
-
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
